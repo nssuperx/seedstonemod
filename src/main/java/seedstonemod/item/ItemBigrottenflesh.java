65043767550a5257fab961434d56f28d5260f3e7
nssuperx <mgtmgtx@gmail.com>
2020-06-13 22:58:45 +0900

    パラメータ調整

--- src/main/java/seedstonemod/item/ItemBigrottenflesh.java
+++ src/main/java/seedstonemod/item/ItemBigrottenflesh.java
@@ -5,9 +5,10 @@ import net.minecraft.potion.Potion;
 import seedstonemod.SeedstoneMod;
 
 public class ItemBigrottenflesh extends ItemFood{
-    public ItemBigrottenflesh(int healAmount, float saturationModifier, Boolean isWolfsFavoriteMeat){
-        super(healAmount, saturationModifier, isWolfsFavoriteMeat);
-        setPotionEffect(Potion.hunger.id, 30, 0, 0.8F);
+    public ItemBigrottenflesh(){
+        // args: int healAmount, float saturationModifier, Boolean isWolfsFavoriteMeat
+        super(8, 1.6F, true);
+        setPotionEffect(Potion.hunger.id, 30, 0, 0.9F);
         setCreativeTab(SeedstoneMod.tabSeedstone);
         setUnlocalizedName("bigRottenFlesh");
         setTextureName("seedstonemod:bigrottenflesh");
