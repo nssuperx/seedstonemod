86d8ac58c4606d6250486d3fce8f4bd49b7e7b2e
nssuperx <40332687+nssuperx@users.noreply.github.com>
2022-01-23 13:54:28 +0900

    fix: 起動できた。modidに大文字があったのがまずかった？

--- src/main/java/seedstonemod/SeedstoneMod.java
+++ src/main/java/seedstonemod/SeedstoneMod.java
@@ -1,41 +1,34 @@
 package seedstonemod;
 
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.Mod.Metadata;
-import net.minecraftforge.fml.common.ModMetadata;
-import net.minecraftforge.fml.common.event.FMLConstructionEvent;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import seedstonemod.item.SeedstoneItems;
-import seedstonemod.block.SeedstoneBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.ModMetadata;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Metadata;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import seedstonemod.block.SeedstoneBlocks;
+import seedstonemod.item.SeedstoneItems;
 
-@Mod(modid = SeedstoneMod.MOD_ID, name = SeedstoneMod.MOD_NAME, version = SeedstoneMod.MOD_VERSION, dependencies = SeedstoneMod.MOD_DEPENDENCIES, acceptedMinecraftVersions = SeedstoneMod.MOD_ACCEPTED_MC_VERSIONS)
-public class SeedstoneMod {
-    public static final String MOD_ID = "seedstoneMod";
+@Mod(modid = SeedstoneMod.MOD_ID, version = SeedstoneMod.MOD_VERSION)
+public class SeedstoneMod
+{
+    public static final String MOD_ID = "seedstonemod";
     public static final String MOD_NAME = "SeedstoneMod";
-    public static final String MOD_VERSION = "1.6.0";
+    public static final String MOD_VERSION = "1.6.1";
     public static final String MOD_DEPENDENCIES = "required-after:Forge@[1.11.2-13.20.1.2588,)";
     public static final String MOD_ACCEPTED_MC_VERSIONS = "[1.11.2]";
-
 
 
     @Metadata(MOD_ID)
     public static ModMetadata meta;
 
     public static CreativeTabs tabSeedstone = new SeedstoneTab("Seedstonetab");
-
-    @Mod.EventHandler
-    public void construct(FMLConstructionEvent event) {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
     
     @SubscribeEvent
     protected static void registerItems(RegistryEvent.Register<Item> event) {
@@ -58,13 +51,9 @@ public class SeedstoneMod {
     public void preInit(FMLPreInitializationEvent event) {
         ModInfo.loadInfo(meta);
     }
-
-
+    
     @EventHandler
     public void Init(FMLInitializationEvent event) {
-
-        // SeedstoneRecipes.registerRecipes();
-
+        SeedstoneRecipes.registerRecipes();
     }
-
 }
